b796350f99f04261150c0eebd67760ae892c948b
Lipécz Ádám <adamlipecz@users.noreply.github.com>
2024-03-08 14:50:57 +0100

    added comments for terminal_handler

--- shrooly_cli/terminal_handler.py
+++ shrooly_cli/terminal_handler.py
@@ -2,20 +2,59 @@ from .constants import PROMPT_REGEX
 from shrooly_cli.serial_handler import serial_trigger_response_type, serial_interface_status, serial_callback_status
 
 class terminal_handler:
+    """
+    A class that handles terminal commands and responses.
+
+    Attributes:
+        waiting_for_terminal_resp (bool): Indicates if the terminal is waiting for a response.
+        terminal_resp_status (str): The status of the terminal response.
+        terminal_resp_payload (str): The payload of the terminal response.
+        serial_handler_instance (object): An instance of the serial handler.
+
+    Methods:
+        __init__(self, serial_handler): Initializes the terminal handler with a serial handler instance.
+        terminal_command_callback(self, status, payload): Callback function for terminal command response.
+        send_command(self, strInput, name="", timeout=5): Sends a command to the terminal and waits for response.
+    """
+
     waiting_for_terminal_resp = False
     terminal_resp_status = ""
     terminal_resp_payload = ""
     serial_handler_instance = None
 
     def __init__(self, serial_handler):
+        """
+        Initializes the terminal handler with a serial handler instance.
+
+        Args:
+            serial_handler (object): An instance of the serial handler.
+        """
         self.serial_handler_instance = serial_handler
 
     def terminal_command_callback(self, status, payload):
+        """
+        Callback function for terminal command response.
+
+        Args:
+            status (str): The status of the terminal response.
+            payload (str): The payload of the terminal response.
+        """
         self.terminal_resp_status = status
         self.terminal_resp_payload = payload
         self.waiting_for_terminal_resp = False
     
     def send_command(self, strInput, name="", timeout=5):
+        """
+        Sends a command to the terminal and waits for response.
+
+        Args:
+            strInput (str): The command to send to the terminal.
+            name (str): The name of the command (optional).
+            timeout (int): The timeout duration in seconds (default is 5).
+
+        Returns:
+            tuple: A tuple containing the status and payload of the terminal response.
+        """
         if self.serial_handler_instance.status is not serial_interface_status.CONNECTED:
             return serial_callback_status.ERROR, ""
         
